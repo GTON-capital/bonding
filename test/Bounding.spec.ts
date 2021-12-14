d385e2fc855dc64a3a1dfe86f1682cadb41ef074
GooGrand <sharkanovnikita@gmail.com>
2021-12-14 22:35:49 +0600

    added change tokens

--- test/Bounding.spec.ts
+++ test/Bounding.spec.ts
@@ -1,6 +1,6 @@
 import { waffle } from "hardhat"
 import { expect } from "./shared/expect"
-import { BigNumber, utils } from 'ethers'
+import { BigNumber, BigNumberish, utils } from 'ethers'
 
 import { boundingFixture } from "./shared/fixtures"
 
@@ -8,7 +8,14 @@ import { Bounding } from "../typechain/Bounding"
 import { WETH9 } from "../typechain/WETH9"
 import { ERC20 } from "../typechain/ERC20"
 import { TestAggregator } from "../typechain/TestAggregator"
+import { TestCan } from "~/typechain/TestCan"
 
+
+interface TokenData {
+  can: TestCan, 
+  price: TestAggregator, 
+  minimalAmount: BigNumberish
+}
 
 describe("Bounding", () => {
   const [wallet, treasury, other] = waffle.provider.getWallets()
@@ -23,10 +30,19 @@ describe("Bounding", () => {
   let token0: ERC20
   let token1: ERC20
   let gton: ERC20
+
   let bounding: Bounding
+
   let token0Agg: TestAggregator
+  let token1Agg: TestAggregator
   let gtonAgg: TestAggregator
   let wethAgg: TestAggregator
+
+  let wethCan: TestCan
+  let token0Can: TestCan
+  let token1Can: TestCan
+
+  let tokens: TokenData[]
 
   beforeEach("deploy test contracts", async () => {
     ; ({
@@ -36,14 +52,20 @@ describe("Bounding", () => {
       token1,
       bounding,
       token0Agg,
+      token1Agg,
       gtonAgg,
-      wethAgg
+      wethAgg,
+      token1Can,
+      token0Can,
+      wethCan
     } = await loadFixture(boundingFixture))
+
+    tokens = [
+      { can: token0Can, price: token0Agg, minimalAmount: BigNumber.from("10000000000000000") },
+      { can: token1Can, price: token1Agg, minimalAmount: BigNumber.from("500000000000000000") },
+      { can: wethCan, price: wethAgg, minimalAmount: BigNumber.from("100000000000000") }
+    ]
   })
-
-  async function setupBounding() {
-
-  }
 
   it("constructor initializes variables", async () => {
     expect(await bounding.owner()).to.eq(wallet.address)
@@ -58,26 +80,23 @@ describe("Bounding", () => {
     expect(await bounding.owner()).to.eq(other.address)
   })
 
-  async function addDiscount(delta: BigNumber, mul: BigNumber, div: BigNumber, min: BigNumber) {
-    await bounding.addDiscount(delta, mul, div, min)
+  async function addDiscount(delta: BigNumber, mul: BigNumber, div: BigNumber) {
+    await bounding.addDiscount(delta, mul, div)
     const last = await bounding.discountsLength();
     const res = await bounding.discounts(last.sub(1));
     expect(res.delta).to.eq(delta)
     expect(res.discountMul).to.eq(mul)
     expect(res.discountDiv).to.eq(div)
-    expect(res.minimalAmount).to.eq(min)
   }
   async function checkDiscount(id: number, discount: {
     delta: BigNumber,
     discountMul: BigNumber,
     discountDiv: BigNumber,
-    minimalAmount: BigNumber
   }) {
     const res = await bounding.discounts(id);
     expect(res.delta).to.eq(discount.delta)
     expect(res.discountMul).to.eq(discount.discountMul)
     expect(res.discountDiv).to.eq(discount.discountDiv)
-    expect(res.minimalAmount).to.eq(discount.minimalAmount)
   }
 
   const discounts = [
@@ -104,15 +123,14 @@ describe("Bounding", () => {
     const delta = BigNumber.from(1)
     const mul = BigNumber.from(12)
     const div = BigNumber.from(1)
-    const min = BigNumber.from(100)
-    await expect(bounding.connect(other).addDiscount(1, 12, 1, 100)).to.be.revertedWith('Bounder: permitted to owner only.')
-    await addDiscount(delta, mul, div, min)
+    await expect(bounding.connect(other).addDiscount(1, 12, 1)).to.be.revertedWith('Bounder: permitted to owner only.')
+    await addDiscount(delta, mul, div)
   })
 
   it("remove discount", async () => {
-    await addDiscount(discounts[0].delta, discounts[0].discountMul, discounts[0].discountDiv, discounts[0].minimalAmount)
-    await addDiscount(discounts[1].delta, discounts[1].discountMul, discounts[1].discountDiv, discounts[1].minimalAmount)
-    await addDiscount(discounts[2].delta, discounts[2].discountMul, discounts[2].discountDiv, discounts[2].minimalAmount)
+    await addDiscount(discounts[0].delta, discounts[0].discountMul, discounts[0].discountDiv)
+    await addDiscount(discounts[1].delta, discounts[1].discountMul, discounts[1].discountDiv)
+    await addDiscount(discounts[2].delta, discounts[2].discountMul, discounts[2].discountDiv)
     expect(await bounding.discountsLength()).to.eq(3);
     await expect(bounding.connect(other).rmDiscount(0)).to.be.revertedWith('Bounder: permitted to owner only.')
     await bounding.rmDiscount(0)
@@ -124,60 +142,73 @@ describe("Bounding", () => {
   })
 
   it("change discount", async () => {
-    await addDiscount(discounts[2].delta, discounts[2].discountMul, discounts[2].discountDiv, discounts[2].minimalAmount)
+    await addDiscount(discounts[2].delta, discounts[2].discountMul, discounts[2].discountDiv)
     await expect(bounding.connect(other).changeDiscount(
       0,
       discounts[1].delta,
       discounts[1].discountMul,
-      discounts[1].discountDiv,
-      discounts[1].minimalAmount)).to.be.revertedWith('Bounder: permitted to owner only.')
+      discounts[1].discountDiv)).to.be.revertedWith('Bounder: permitted to owner only.')
 
     await bounding.changeDiscount(
       0,
       discounts[1].delta,
       discounts[1].discountMul,
-      discounts[1].discountDiv,
-      discounts[1].minimalAmount)
+      discounts[1].discountDiv)
 
     await checkDiscount(0, discounts[1])
   })
 
-  async function addToken(token: {canAddress: string, priceAgg: string}) {
-    await bounding.addAllowedToken(delta, mul, div, min)
-    const last = await bounding.discountsLength();
-    const res = await bounding.discounts(last.sub(1));
-    expect(res.delta).to.eq(delta)
-    expect(res.discountMul).to.eq(mul)
-    expect(res.discountDiv).to.eq(div)
-    expect(res.minimalAmount).to.eq(min)
+  async function checkToken(id: BigNumberish, { can, price, minimalAmount }: TokenData) {
+    const token = await bounding.allowedTokens(id)
+    const tokenAddress = await can.token();
+    expect(token.price).to.eq(price.address)
+    expect(token.token).to.eq(tokenAddress)
+    expect(token.can).to.eq(can.address)
+    expect(token.minimalAmount).to.eq(minimalAmount)
   }
 
+  async function addToken({ can, price, minimalAmount }: TokenData) {
+    await bounding.addAllowedToken(price.address, can.address, minimalAmount)
+    const last = await bounding.tokensLength();
+    await checkToken(last.sub(1), {can, price, minimalAmount})
+  }
 
   it("add token", async () => {
-
+    for (const item of tokens) {
+      await addToken(item)
+    }
   })
 
   it("remove token", async () => {
+    const token = tokens[0]
+    await expect(bounding.connect(other)
+      .addAllowedToken(token.price.address, token.can.address, token.minimalAmount))
+      .to.be.revertedWith("Bounder: permitted to owner only.");
+    for (const item of tokens) {
+      await addToken(item)
+    }
+    await expect(bounding.connect(other).rmAllowedToken(0)).to.be.revertedWith("Bounder: permitted to owner only.");
+    expect(await bounding.tokensLength()).to.eq(tokens.length);
+    await bounding.rmAllowedToken(0)
 
+    // expect 3rd element to be swapped in place of 1st
+    await checkToken(0, tokens[2])
+    // expect 3rd element to be deleted
+    await expect(bounding.allowedTokens(2)).to.be.reverted
   })
 
   it("change token", async () => {
-    await addDiscount(discounts[2].delta, discounts[2].discountMul, discounts[2].discountDiv, discounts[2].minimalAmount)
-    await expect(bounding.connect(other).changeDiscount(
-      0,
-      discounts[1].delta,
-      discounts[1].discountMul,
-      discounts[1].discountDiv,
-      discounts[1].minimalAmount)).to.be.revertedWith('Bounder: permitted to owner only.')
+    const token0 = tokens[0]
+    const token1 = tokens[1]
+    await addToken(token0)
+    const last = await bounding.tokensLength();
 
-    await bounding.changeDiscount(
-      0,
-      discounts[1].delta,
-      discounts[1].discountMul,
-      discounts[1].discountDiv,
-      discounts[1].minimalAmount)
+    await expect(bounding.connect(other)
+      .changeAllowedToken(last.sub(1), token1.price.address, token1.can.address, token1.minimalAmount))
+      .to.be.revertedWith("Bounder: permitted to owner only.");
 
-    await checkToken(0, discounts[1])
+    await bounding.changeAllowedToken(last.sub(1), token1.price.address, token1.can.address, token1.minimalAmount)
+    checkToken(last.sub(1), token1)
   })
 
   it("get token amount with discount", async () => {
--- test/shared/fixtures.ts
+++ test/shared/fixtures.ts
@@ -6,6 +6,7 @@ import { Bounding } from "../../typechain/Bounding"
 import { WETH9 } from "../../typechain/WETH9"
 import { ERC20 } from "../../typechain/ERC20"
 import { TestAggregator } from "../../typechain/TestAggregator"
+import { TestCan } from "../../typechain/TestCan"
 
 interface TokensFixture {
     weth: WETH9,
@@ -30,6 +31,10 @@ interface Boundingfixture extends TokensFixture {
     token0Agg: TestAggregator
     gtonAgg: TestAggregator
     wethAgg: TestAggregator
+    token1Agg: TestAggregator
+    wethCan: TestCan
+    token0Can: TestCan
+    token1Can: TestCan
 }
 
 export const boundingFixture: Fixture<Boundingfixture> = async function ([
@@ -37,9 +42,14 @@ export const boundingFixture: Fixture<Boundingfixture> = async function ([
 ]): Promise<Boundingfixture> {
     const { weth, token0, token1, gton } = await tokensFixture()
     const aggFactory = await ethers.getContractFactory("TestAggregator");
-    const token0Agg = (await aggFactory.deploy(await token0.decimals(), 1)) as TestAggregator
+    const token0Agg = (await aggFactory.deploy(8, 50000000)) as TestAggregator
+    const token1Agg = (await aggFactory.deploy(8, 25400000)) as TestAggregator
     const gtonAgg = (await aggFactory.deploy(8, 1000000000)) as TestAggregator
     const wethAgg = (await aggFactory.deploy(8, 1000000000)) as TestAggregator
+    const canFactory = await ethers.getContractFactory("TestCan")
+    const token0Can = (await canFactory.deploy(token0.address)) as TestCan;
+    const token1Can = (await canFactory.deploy(token1.address)) as TestCan;
+    const wethCan = (await canFactory.deploy(weth.address)) as TestCan;
     const bounfingF = await ethers.getContractFactory("Bounding")
     const bounding = (await bounfingF.deploy(gton.address, treasury.address)) as Bounding
     return {
@@ -48,9 +58,13 @@ export const boundingFixture: Fixture<Boundingfixture> = async function ([
         token1,
         gton,
         token0Agg,
+        token1Agg,
         wethAgg,
         bounding,
-        gtonAgg
+        gtonAgg,
+        wethCan,
+        token0Can,
+        token1Can
     }
 }
 
