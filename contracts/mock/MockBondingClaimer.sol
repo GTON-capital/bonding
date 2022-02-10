6d9d18f0377e2390215d147a40da425c0ffb53c6
GooGrand <sharkanovnikita@gmail.com>
2022-02-11 03:42:15 +0600

    Update: add aggregator mock, change versioning

--- contracts/mock/MockBondingClaimer.sol
+++ contracts/mock/MockBondingClaimer.sol
@@ -3,8 +3,9 @@ pragma solidity >=0.8.0;
 
 import { IBondingClaimer } from "../interfaces/IBondingClaimer.sol";
 import { IBondStorage } from "../interfaces/IBondStorage.sol";
+import { ERC721Holder } from "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 
-contract BondingClaimer is IBondingClaimer {
+contract MockBondingClaimer is IBondingClaimer, ERC721Holder {
 
     constructor(IBondStorage _bondStorage) {
         bondStorage = _bondStorage;
